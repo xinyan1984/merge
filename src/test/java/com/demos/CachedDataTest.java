e43a3da82c3b80653b01c98e2e54fb22e70a926f
xin.yang <xin.yang>
2020-04-14 18:24:37 +0800

    source codes

---
+++ src/test/java/com/demos/CachedDataTest.java
@@ -0,0 +1,12 @@
+package com.demos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CachedDataTest {
+
+  @Test
+  public void processCachedData() {
+  }
+}
---
+++ src/test/java/com/demos/StackDemoTest.java
@@ -0,0 +1,24 @@
+package com.demos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StackDemoTest {
+
+  @Test
+  public void push() {
+  }
+
+  @Test
+  public void peek() {
+  }
+
+  @Test
+  public void pop() {
+  }
+
+  @Test
+  public void empty() {
+  }
+}
