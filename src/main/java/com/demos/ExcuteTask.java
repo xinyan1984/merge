690a4ed84868055a440554993d654404a381db66
xin.yang <xin.yang>
2020-04-14 16:55:05 +0800

    source 凑得上

--- src/main/java/com/demos/ExcuteTask.java
+++ src/main/java/com/demos/ExcuteTask.java
@@ -1,26 +1,72 @@
 package com.demos;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Stack;
 
 public class ExcuteTask {
 
-  List<PointThread> list = new ArrayList<PointThread>();
+  List<PointThread> listArray = new ArrayList<PointThread>();
+  HashMap<String, PointThread> hashMap = new HashMap<String, PointThread>();
 
   //二维数组存放节点
-  PointThread[][] ss = new PointThread[2][5];
 
 
   public void initData() {
-    String initCharater = "A";
-    for (int i = 0; i < 8; i++) {
-      list.add(new PointThread(initCharater + i));
+    Stack<PointThread> stackInit = new Stack<PointThread>();
+
+    PointThread A = new PointThread("A");
+    PointThread B = new PointThread("B");
+    PointThread C = new PointThread("C");
+    PointThread D = new PointThread("D");
+    PointThread E = new PointThread("E");
+    PointThread F = new PointThread("F");
+    PointThread G = new PointThread("G");
+
+    listArray.add(A);
+    stackInit.push(G);
+    stackInit.push(C);
+    A.previous = stackInit;
+
+    stackInit.removeAllElements();
+
+    listArray.add(B);
+    listArray.add(C);
+    listArray.add(D);
+    listArray.add(E);
+    listArray.add(F);
+  }
+
+  public void excuteTask() {
+    int max = listArray.size();
+    Stack<PointThread> stack = new Stack<PointThread>();
+    for (int i = 0; i < max; i++) {
+      PointThread tmpPoint = listArray.get(i);
+      if (tmpPoint.previous == null) {
+        stack.push(tmpPoint);
+      }
     }
 
-    for (int i = 0; i < 2; i++) {
 
+    do {
+      Stack<PointThread> stacknew = new Stack<PointThread>();
+      PointThread tmp = stack.pop();
+      if (tmp.previous == null) {
+        tmp.run();
+        PointThread tmpNext = tmp.next.peek();
+        tmpNext.previous = null;
+        tmp = null;
+        if(tmpNext.next.size()==1) {
+          stacknew.push(tmp);
+        }
+        else{
 
-    }
+        }
+      }
+      stack = stacknew;
+    } while (!stack.isEmpty());
+
 
   }
 
--- src/main/java/com/demos/PointThread.java
+++ src/main/java/com/demos/PointThread.java
@@ -1,24 +1,36 @@
 package com.demos;
 
-public class PointThread implements Runnable{
-  public int lenth;
-  public PointThread first;
-  public PointThread second;
+import java.util.Stack;
+
+public class PointThread {
+
+  public Stack<PointThread> getNext() {
+    return next;
+  }
+
+  public void setNext(Stack<PointThread> next) {
+    this.next = next;
+  }
+
+  public Stack<PointThread> getPrevious() {
+    return previous;
+  }
+
+  public void setPrevious(Stack<PointThread> previous) {
+    this.previous = previous;
+  }
+
+  public Stack<PointThread> next;
+  public Stack<PointThread> previous;
   private String threadName = "";
 
-  public PointThread(int lenth, PointThread first, PointThread second) {
-    this.lenth = lenth;
-    this.first = first;
-    this.second = second;
-  }
 
   public PointThread(String threadName) {
     this.threadName = threadName;
   }
 
-  @Override
+
   public void run() {
     System.out.println(threadName);
-
   }
 }
